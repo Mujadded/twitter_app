73e812892f60165363c0b7dee01d07fa28ea0fa9
Mujadded <mujadded.alif@gmail.com>
2017-11-02 09:52:52 +0000

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  
+  #test "invalid signup informations" do
+   # get signup_path
+    #assert_no_difference 'User.count' do
+     # post users_path, params: { user: { name: "", email: "user@invalid"  }
+end
